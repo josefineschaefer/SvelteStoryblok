822491c836b0c6104152d0ec3d73ac5c8811dab1
josefineschaefer <josefine.schaefer@storyblok.com>
2022-08-23 12:37:24 +0200

    adding components and tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+    import { useStoryblokBridge, StoryblokComponent } from "@storyblok/svelte";
+
+    export let data;
+
+    onMount(() => {
+        useStoryblokBridge(
+            data.story.id,
+            (newStory) => (data.story = newStory)
+        );
+    });
+</script>
+
+<div>
+    {#if data.story}
+        <StoryblokComponent blok={data.story.content} />
+    {/if}
+</div>
